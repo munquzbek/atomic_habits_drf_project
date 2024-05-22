9e1a9a9a4b2f4c158ae61f6b86d00b4e7146d031
munquzbek <140368524+munquzbek@users.noreply.github.com>
2024-05-22 06:50:40 +0600

    +validator to check field is_good true(add related_habit) or false(add reward)

--- habits/serializers.py
+++ habits/serializers.py
@@ -7,3 +7,16 @@ class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, data):
+        """
+        check if field is_good is true can add only field related_habit
+        if field is_good is false user can add only field reward
+        """
+        if data['is_good']:
+            if data['reward']:
+                raise serializers.ValidationError({'reward': 'Change habit to is good to add reward'})
+        elif not data['is_good']:
+            if data['related_habit']:
+                raise serializers.ValidationError({'related_habit': 'Change habit to is not good to add related_habit'})
+        return data
